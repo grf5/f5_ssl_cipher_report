678df20a0894dec6178b751a410105a0fbdeb5af
g.robinson@f5.com <Seattle2016!>
2020-06-12 16:45:03 -0400

    fixing csv

--- f5_ssl_scan.py
+++ f5_ssl_scan.py
@@ -146,15 +146,16 @@ def create_ssl_csv(host, username, password, csvfile, CLIENT_CIPHER_DICT, SERVER
         report_writer = csv.writer(outputcsv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         report_writer.writerow(['VIP NAME','CLIENT SSL PROFILE','PARENT CLIENT SSL PROFILE', 'SERVER SSL PROFILE','PARENT SERVER SSL PROFILE'])
         for current_virtual in LTM_VIRTUAL_LIST:
-            current_client_profile = ''
-            current_client_parent_profile = ''
-            current_server_profile = ''
-            current_server_parent_profile = ''
+            current_client_profile = 'none'
+            current_client_parent_profile = 'none'
+            current_server_profile = 'none'
+            current_server_parent_profile = 'none'
             if current_virtual['subPath']:
                 api_response = icontrol_get(host, username, password, '/ltm/virtual/~' + current_virtual['partition'] + '~' + current_virtual['subPath'] + '~' + current_virtual['name'] + '/profiles')
             else:
                 api_response = icontrol_get(host, username, password, '/ltm/virtual/~' + current_virtual['partition'] + '~' + current_virtual['name'] + '/profiles')
             api_response_dict = json.loads(api_response)
+            print(api_response_dict)
             current_virtual_profiles = api_response_dict['items']
             for current_virtual_profile in current_virtual_profiles:
                 if current_virtual_profile['context'] == 'clientside' and current_virtual_profile['name'] in CLIENT_CIPHER_DICT:
